50543ef5f3a80b1528a2635664b9ce8b8a444cbb
“Aaron <amineen64@hotmail.com>
2025-06-12 16:50:44 +0000

    Implement errorResponse function to handle JSON error responses

--- cmd/api/error.go
+++ cmd/api/error.go
@@ -1,7 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	envelope := map[string]any{
+		"success": false,
+		"data":    nil,
+		"msg":     message,
+	}
+	js, err := json.Marshal(envelope)
+	if err != nil {
+		app.logger.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
 }
